44051ea0a9edb98cf9e2e4537ab0d631f243e2cc
‘nydia’ <nydia_lhq@hotmail.com>
2022-07-20 23:38:24 +0800

    add

--- rust_learning/src/main.rs
+++ rust_learning/src/main.rs
@@ -474,14 +474,30 @@ fn common_collections_test(){
         let str2 = String::from("it is str");
 
         let data = "initial contents";
-        let s = data.to_string();
+        let s1 = data.to_string();
 
-    // 该方法也可直接用于字符串字面值：
-    let s = "initial contents".to_string();
-    }
+        let s2 = String::from("initial contents");
+
+        let mut s3 = String::from("foo");
+        let s4 = "bar";
+        s3.push_str(s4);
+        println!("s3 is {}", s3);
+
+        let s4 = String::from("Hello, ");
+        let s5 = String::from("world!");
+        let s6 = s1 + &s2; // 注意 s1 被移动了，不能继续使用
+
+        // 该方法也可直接用于字符串字面值：
+        let s6 = "initial contents".to_string();
+        }
     //hashmap
+    use std::collections::HashMap;
     fn common_collections_hashmap_test(){
+        let mut scores = HashMap::new();
 
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+        
     }
    
 }
