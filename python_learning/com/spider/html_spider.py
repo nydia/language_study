e802af0ae766c75f7ba75eac5031672516ffc4ea
nydia <nydia_lvhq@sina.cn>
2023-06-19 18:51:26 +0800

    update new files

---
+++ python_learning/com/spider/html_spider.py
@@ -0,0 +1,22 @@
+import urllib.request;
+
+
+def getHtml(url):  # 获取html的内容
+    html = urllib.request.urlopen(url).read()  # bytes 如果不用read()，html会是一个↓
+    return html                                # http.client.HTTPResponse的变量
+
+
+def saveHtml(fileName, fileContent):
+    with open(fileName, "wb") as f:  # 以wb打开文件
+        f.write(fileContent)  # 写入
+
+
+def main():
+    url = "https://www.zhihuishu.com/"  # url
+    html = getHtml(url)  # 调用函数获取bytes
+    saveHtml("C:\temp\javashop.html", html)  # 保存
+    print("保存网页完成")  # 提示语
+
+
+if __name__ == "__main__":  # 主函数
+    main()
