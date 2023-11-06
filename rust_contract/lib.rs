770a3c7f519b51e607d0b86e7fac5cf688ea62c3
nydia <nydia_lvhq@sina.cn>
2023-11-06 19:27:52 +0800

    update new files

--- rust_contract/lib.rs
+++ rust_contract/lib.rs
@@ -1,74 +1,100 @@
+// If the `std` feature from the `Cargo.toml` is not enabled
+// we switch on `no_std`, this has the effect of Rusts standard
+// library not being included in our contract.
+//
+// The Rust standard library is OS-dependent and Wasm is
+// architecture independent.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_lang as ink;
-
+// This is the ink! macro, the starting point for your contract.
+// Everything below it might look like Rust code, but it is actually
+// run through a parser in ink!.
 #[ink::contract]
-mod rust_contract {
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+pub mod flipper {
+    /// This is the contract's storage.
     #[ink(storage)]
-    pub struct RustContract {
-        /// Stores a single `bool` value on the storage.
+    pub struct Flipper {
         value: bool,
     }
 
-    impl RustContract {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    impl Flipper {
+        /// A constructor that the contract can be initialized with.
         #[ink(constructor)]
         pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+            /* --snip-- */
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
+        /// An alternative constructor that the contract can be
+        /// initialized with.
         #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
+        pub fn new_default() -> Self {
+            /* --snip-- */
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// A state-mutating function that the contract exposes to the
+        /// outside world.
+        ///
+        /// By default functions are private, they have to be annotated
+        /// with `#[ink(message)]` and `pub` to be available from the
+        /// outside.
         #[ink(message)]
         pub fn flip(&mut self) {
-            self.value = !self.value;
+            /* --snip-- */
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// A public contract function that has no side-effects.
+        ///
+        /// Note that while purely reading functions can be invoked
+        /// by submitting a transaction on-chain, this is usually
+        /// not done as they have no side-effects and the transaction
+        /// costs would be wasted.
+        /// Instead those functions are typically invoked via RPC to
+        /// return a contract's state.
         #[ink(message)]
         pub fn get(&self) -> bool {
-            self.value
+            /* --snip-- */
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
+        /// This attribute denotes that the test is executed in
+        /// a simulated, mocked blockchain environment. There are
+        /// functions available to influence how the test environment
+        /// is configured (e.g. setting an account to a specified balance).
         #[ink::test]
         fn default_works() {
-            let rust_contract = RustContract::default();
-            assert_eq!(rust_contract.get(), false);
+            /* --snip-- */
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut rust_contract = RustContract::new(false);
-            assert_eq!(rust_contract.get(), false);
-            rust_contract.flip();
-            assert_eq!(rust_contract.get(), true);
+        /* --snip-- */
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use super::*;
+        use ink_e2e::build_message;
+
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        /// With this attribute the contract will be compiled and deployed
+        /// to a Substrate node that is required to be running in the
+        /// background.
+        ///
+        /// We offer API functions that enable developers to then interact
+        /// with the contract. ink! will take care of putting contract calls
+        /// into transactions that will be submitted to the Substrate chain.
+        ///
+        /// Developers can define assertions on the outcome of their transactions,
+        /// such as checking for state mutations, transaction failures or
+        /// incurred gas costs.
+        #[ink_e2e::test]
+        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            /* --snip-- */
         }
+
+        /* --snip-- */
     }
 }
