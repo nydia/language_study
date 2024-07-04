485bea18d4c8c6c047acc46fa54c151fceeed775
‘nydia’ <nydia_lhq@hotmail.com>
2024-07-04 23:56:41 +0800

    去除水印

--- nydia_tools/tools/views.py
+++ nydia_tools/tools/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .utils import file_utils
 from .utils import json_utils
+from .utils.watermark_utils import watermark_util
+from django.core import serializers
+from django.http import HttpResponse
 
 # Create your views here.
 
+# 工具首页
 def tools_index(request):
     context = {}
     return render(request, "tools/tools_index.html", context, None, None, None)
@@ -14,3 +18,14 @@ def tools_index2(request):
     print("------------------")
     print(context)
     return render(request, "tools/tools_index2.html", context, None, None, None)
+
+# 去除水印页面
+def tools_watermark(request):
+    return render(request, "tools/tools_watermark.html", '', None, None, None)
+
+# 去除水印操作
+def watermark(request):
+    watermark_util.remove_watermark('C:/temp/1.jpg','C:/temp/2.jpg')
+    scripts = Scripts.objects.all()[0:1]
+    json_data = serializers.serialize('json', scripts)
+    return HttpResponse(json_data, content_type="application/json")
