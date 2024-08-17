79ad416986105f1a1afb75467888496f01638d90
‘nydia’ <nydia_lhq@hotmail.com>
2024-08-18 01:04:35 +0800

    redis

--- python_learning/middleware/redis_data_struct_test.py
+++ python_learning/middleware/redis_data_struct_test.py
@@ -16,12 +16,9 @@ data = {
 for key, value in data.items():
     r.set(key, value[0])
 memory_strings = r.memory_usage(key)
+# 字符串存储-编码
+strings_encoding = r.execute_command('OBJECT', 'ENCODING', key)
 
-script = """
-    return redis.call('object encoding key', KEYS[1])
-"""
-result = r.eval(script, 1, 'key')
-print("key:",result)
 
 
 # 列表存储
@@ -29,12 +26,20 @@ for key, value in data.items():
     r.delete(key)
     r.rpush(key, *value)
 memory_lists = r.memory_usage(key)
+# 列表存储-编码
+lists_encoding = r.execute_command('OBJECT', 'ENCODING', key)
+
+
 
 # 集合存储
 for key, value in data.items():
     r.delete(key)
     r.sadd(key, *value)
 memory_sets = r.memory_usage(key)
+# 集合存储-编码
+sets_encoding = r.execute_command('OBJECT', 'ENCODING', key)
+
+
 
 # 有序集合存储
 for key, value in data.items():
@@ -42,12 +47,19 @@ for key, value in data.items():
     for v in value:
         r.zadd(key, {v: random.random()})
 memory_sorted_sets = r.memory_usage(key)
+# 有序集合存储-编码
+sorted_sets_encoding = r.execute_command('OBJECT', 'ENCODING', key)
+
+
 
 # 哈希表存储
 for key, value in data.items():
     r.delete(key)
     r.hset(key, mapping={str(i): v for i, v in enumerate(value)})
 memory_hashes = r.memory_usage(key)
+# 哈希表存储-编码
+hashes_encoding = r.execute_command('OBJECT', 'ENCODING', key)
+
 
 # 输出结果
 print("Memory usage for Strings:", memory_strings)
@@ -56,4 +68,8 @@ print("Memory usage for Sets:", memory_sets)
 print("Memory usage for Sorted Sets:", memory_sorted_sets)
 print("Memory usage for Hashes:", memory_hashes)
 
-#print("Encoding usage for Strings:", encoding_strings)
+print("Encoding for Strings:", strings_encoding)
+print("Encoding usage for Lists:", lists_encoding)
+print("Encoding usage for Sets:", sets_encoding)
+print("Encoding usage for Sorted Sets:", sorted_sets_encoding)
+print("Encoding usage for Hashes:", hashes_encoding)
