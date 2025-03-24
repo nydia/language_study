a46391e6d55902928201e2e75b59fd1720045ea5
nydia <nydia_lhq@hotmail.com>
2025-03-25 00:21:50 +0800

    file storage

--- nydia_tools/files/utils/file_constants.py
+++ nydia_tools/files/utils/file_constants.py
@@ -3,6 +3,10 @@ from files.utils.file_enum import StorageTypeEnum
 
 """
 文件模块 - 类定义
+UPLOAD_GITEE_HOST = 'http://127.0.0.1:8081/upload' 本地开发使用
+ULOAD_GITEE_HOST = 'http://127.0.0.1:81/upload' pm内部访问自己的地址(nginx禁止访问pm项目)
+UPLOAD_GITEE_HOST = 'http://172.17.0.1:81/upload' docker容器内部访问pm项目使用地址(nginx禁止访问pm项目)
+UPLOAD_GITEE_HOST = 'http://www.aith.top/api/upload' 使用外网地址访问(nginx可以访问pm项目)
 """
 
 UPLOAD_TYPE = UploadTypeEnum.FORM
